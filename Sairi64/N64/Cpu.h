e9b989c816f1e1e687150a670ef1799a4b37e565
sashi0034 <satoshi3104.nishi@gmail.com>
2023-08-06 22:53:16 +0900

    bit操作用の関数を追加

---
+++ Sairi64/N64/Cpu.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+namespace N64
+{
+	class Cpu
+	{
+	public:
+	};
+}
---
+++ Sairi64/Utils/Util.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+namespace Utils
+{
+	template <int x1, int x2, typename T>
+	T GetBits(T value)
+	{
+		static_assert(std::is_integral<T>::value, "Type must be an integral type");
+		static_assert(x1 <= x2 && x2 < std::numeric_limits<T>::digits);
+
+		T mask = ((T(1) << (x2 - x1 + 1)) - 1) << x1;
+		return (value & mask) >> x1;
+	}
+
+	template <int x1, int x2, typename T>
+	T SetBits(T value, T newBits)
+	{
+		static_assert(std::is_integral<T>::value, "Type must be an integral type");
+		static_assert(x1 <= x2 && x2 < std::numeric_limits<T>::digits);
+
+		T mask = ((T(1) << (x2 - x1 + 1)) - 1);
+		newBits &= mask;
+		mask <<= x1;
+
+		return (value & ~mask) | ((newBits << x1) & mask);
+	}
+}

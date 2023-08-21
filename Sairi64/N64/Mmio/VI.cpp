5a7f2ea7dde7c21c2fd6f7a12745b2bd710ad216
sashi0034 <satoshi3104.nishi@gmail.com>
2023-08-21 21:53:14 +0900

    VI仮追加

---
+++ Sairi64/N64/Mmio/VI.cpp
@@ -0,0 +1,84 @@
+﻿#include "stdafx.h"
+#include "VI.h"
+
+#include "N64/N64Logger.h"
+
+namespace N64::Mmio
+{
+	uint32 VI::Read32(PAddr32 paddr) const
+	{
+		switch (paddr)
+		{
+		case ViAddress::Status_0x04400000:
+			break;
+		case ViAddress::Origin_0x04400004:
+			break;
+		case ViAddress::Width_0x04400008:
+			break;
+		case ViAddress::VInterrupt_0x0440000C:
+			break;
+		case ViAddress::VCurrent_0x04400010:
+			break;
+		case ViAddress::Burst_0x04400014:
+			break;
+		case ViAddress::VSync_0x04400018:
+			break;
+		case ViAddress::HSync_0x0440001C:
+			break;
+		case ViAddress::HSyncLeap_0x04400020:
+			break;
+		case ViAddress::HVideo_0x04400024:
+			break;
+		case ViAddress::VVideo_0x04400028:
+			break;
+		case ViAddress::VBurst_0x0440002C:
+			break;
+		case ViAddress::XScale_0x04400030:
+			break;
+		case ViAddress::YScale_0x04400034:
+			break;
+		default: break;
+		}
+
+		N64Logger::Abort(U"unsupported vi read: paddr={:08X}"_fmt(static_cast<uint32>(paddr)));
+		return 0;
+	}
+
+	void VI::Write32(N64System& n64, PAddr32 paddr, uint32 value)
+	{
+		switch (paddr)
+		{
+		case ViAddress::Status_0x04400000:
+			break;
+		case ViAddress::Origin_0x04400004:
+			break;
+		case ViAddress::Width_0x04400008:
+			break;
+		case ViAddress::VInterrupt_0x0440000C:
+			break;
+		case ViAddress::VCurrent_0x04400010:
+			break;
+		case ViAddress::Burst_0x04400014:
+			break;
+		case ViAddress::VSync_0x04400018:
+			break;
+		case ViAddress::HSync_0x0440001C:
+			break;
+		case ViAddress::HSyncLeap_0x04400020:
+			break;
+		case ViAddress::HVideo_0x04400024:
+			break;
+		case ViAddress::VVideo_0x04400028:
+			break;
+		case ViAddress::VBurst_0x0440002C:
+			break;
+		case ViAddress::XScale_0x04400030:
+			break;
+		case ViAddress::YScale_0x04400034:
+			break;
+		default: break;
+		}
+
+		N64Logger::Abort(U"unsupported vi write: paddr={:08X}"_fmt(static_cast<uint32>(paddr)));
+	}
+}
---
+++ Sairi64/N64/Mmio/VI.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include "N64/Forward.h"
+
+namespace N64::Mmio
+{
+	namespace ViAddress
+	{
+		constexpr PAddr32 Status_0x04400000{0x04400000};
+		constexpr PAddr32 Origin_0x04400004{0x04400004};
+		constexpr PAddr32 Width_0x04400008{0x04400008};
+		constexpr PAddr32 VInterrupt_0x0440000C{0x0440000C};
+		constexpr PAddr32 VCurrent_0x04400010{0x04400010};
+		constexpr PAddr32 Burst_0x04400014{0x04400014};
+		constexpr PAddr32 VSync_0x04400018{0x04400018};
+		constexpr PAddr32 HSync_0x0440001C{0x0440001C};
+		constexpr PAddr32 HSyncLeap_0x04400020{0x04400020};
+		constexpr PAddr32 HVideo_0x04400024{0x04400024};
+		constexpr PAddr32 VVideo_0x04400028{0x04400028};
+		constexpr PAddr32 VBurst_0x0440002C{0x0440002C};
+		constexpr PAddr32 XScale_0x04400030{0x04400030};
+		constexpr PAddr32 YScale_0x04400034{0x04400034};
+	}
+
+	// https://n64brew.dev/wiki/Video_Interface
+	// Video Interface
+	class VI
+	{
+	public:
+		uint32 Read32(PAddr32 paddr) const;
+		void Write32(N64System& n64, PAddr32 paddr, uint32 value);
+
+	private:
+		uint32 m_status{};
+		uint32 m_origin{};
+		uint32 m_width{320};
+		uint32 m_vInterrupt{256};
+		uint32 m_vCurrent{};
+		uint32 m_burst{};
+		uint32 m_vSync{};
+		uint32 m_hSync{};
+		uint32 m_hSyncLeap{};
+		uint32 m_hVideo{};
+		uint32 m_vVideo{};
+		uint32 m_vBurst{};
+		uint32 m_xScale{};
+		uint32 m_yScale{};
+
+		int m_numHalfLines{262};
+		int m_nmFields{1};
+		int m_cyclesPerHalfLine{1000};
+	};
+}

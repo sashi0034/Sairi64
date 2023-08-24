4890adb47d3e176a856368b359e83918de947fd1
sashi0034 <satoshi3104.nishi@gmail.com>
2023-08-24 14:38:19 +0900

    通るテスト

--- Sairi64/Tests/N64Test.cpp
+++ Sairi64/Tests/N64Test.cpp
@@ -64,9 +64,9 @@ namespace Tests
 		REQUIRE(Dillon64Test(U"dsll_simpleboot.z64"));
 		REQUIRE(Dillon64Test(U"dsllv_simpleboot.z64"));
 		REQUIRE(Dillon64Test(U"dsra32_simpleboot.z64"));
-		REQUIRE(Dillon64Test(U"nor_simpleboot.z64"));
-		REQUIRE(Dillon64Test(U"or_simpleboot.z64"));
-		REQUIRE(Dillon64Test(U"ori_simpleboot.z64"));
+		// REQUIRE(Dillon64Test(U"nor_simpleboot.z64")); // OK
+		// REQUIRE(Dillon64Test(U"or_simpleboot.z64")); // OK
+		// REQUIRE(Dillon64Test(U"ori_simpleboot.z64")); // OK
 		// REQUIRE(Dillon64Test(U"sll_simpleboot.z64")); // OK
 		REQUIRE(Dillon64Test(U"sllv_simpleboot.z64"));
 		REQUIRE(Dillon64Test(U"slt_simpleboot.z64"));
@@ -75,11 +75,11 @@ namespace Tests
 		REQUIRE(Dillon64Test(U"sltu_simpleboot.z64"));
 		REQUIRE(Dillon64Test(U"sra_simpleboot.z64"));
 		REQUIRE(Dillon64Test(U"srav_simpleboot.z64"));
-		REQUIRE(Dillon64Test(U"srl_simpleboot.z64"));
+		// REQUIRE(Dillon64Test(U"srl_simpleboot.z64")); // OK
 		REQUIRE(Dillon64Test(U"srlv_simpleboot.z64"));
 		REQUIRE(Dillon64Test(U"subu_simpleboot.z64"));
-		REQUIRE(Dillon64Test(U"xor_simpleboot.z64"));
-		REQUIRE(Dillon64Test(U"xori_simpleboot.z64"));
+		// REQUIRE(Dillon64Test(U"xor_simpleboot.z64")); // OK
+		// REQUIRE(Dillon64Test(U"xori_simpleboot.z64")); // OK
 	}
 }
 #endif

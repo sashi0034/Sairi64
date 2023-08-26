64827147ca56824a258933fe842224764b5f4525
sashi0034 <satoshi3104.nishi@gmail.com>
2023-08-26 11:19:52 +0900

    バイト配列アクセスをリトルエンディアンに修正

--- Sairi64/Tests/UtilTest.cpp
+++ Sairi64/Tests/UtilTest.cpp
@@ -49,4 +49,41 @@ namespace Tests
 		REQUIRE(access4_5.Get() == 0b01);
 		REQUIRE(bit == 0b0101'0000'1101'0011);
 	}
+
+	TEST_CASE("UtilTest_ReadBytes")
+	{
+		std::vector<uint8_t> data = {0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+
+		// 32-bitの読み取り
+		const uint32_t read32 = ReadBytes<uint32_t>(data, 4);
+		REQUIRE(read32 == 0x04030201);
+
+		// 64-bitの読み取り
+		const uint64_t read64 = ReadBytes<uint64_t>(data, 4);
+		REQUIRE(read64 == 0x04030201'08070605);
+	}
+
+	TEST_CASE("UtilTest_WriteBytes")
+	{
+		std::vector<uint8_t> data(32);
+
+		// 32-bitの書き込み
+		WriteBytes(data, 4, static_cast<uint32_t>(0x04030201));
+		REQUIRE(data[4] == 0x01);
+		REQUIRE(data[5] == 0x02);
+		REQUIRE(data[6] == 0x03);
+		REQUIRE(data[7] == 0x04);
+
+		// 64-bitの書き込み
+		WriteBytes(data, 8, static_cast<uint64_t>(0x0807060504030201));
+		REQUIRE(data[8 + 0] == 0x05);
+		REQUIRE(data[8 + 1] == 0x06);
+		REQUIRE(data[8 + 2] == 0x07);
+		REQUIRE(data[8 + 3] == 0x08);
+		REQUIRE(data[8 + 4] == 0x01);
+		REQUIRE(data[8 + 5] == 0x02);
+		REQUIRE(data[8 + 6] == 0x03);
+		REQUIRE(data[8 + 7] == 0x04);
+		REQUIRE(ReadBytes64(data, 8) == 0x0807060504030201);
+	}
 }
